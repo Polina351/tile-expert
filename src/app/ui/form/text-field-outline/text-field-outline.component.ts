4a3b7bceec12a4b87ac2cead83483fb8672b00a1
Polina Deniskina <polinaoioi67@gmail.com>
2025-05-06 13:12:12 +0300

    feat: add filter responsive

--- src/app/ui/form/text-field-outline/text-field-outline.component.ts
+++ src/app/ui/form/text-field-outline/text-field-outline.component.ts
@@ -1,12 +1,16 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, input, InputSignal} from '@angular/core';
+import {ButtonMeComponent} from '../../buttons/button-me/button-me.component';
 
 @Component({
-  selector: 'app-text-field-outline',
-  imports: [],
-  templateUrl: './text-field-outline.component.html',
-  styleUrl: './text-field-outline.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-text-field-outline',
+    imports: [
+        ButtonMeComponent
+    ],
+    templateUrl: './text-field-outline.component.html',
+    styleUrl: './text-field-outline.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TextFieldOutlineComponent {
-
+    legend: InputSignal<string> = input<string>('Автор');
+    placeholder: InputSignal<string> = input<string>('Имя или должность');
 }
